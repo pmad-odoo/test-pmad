5c59761b204aaabff888286f187026a493c70319
pmad-odoo <pmad@odoo.com>
2026-02-12 10:35:24 +0530

    [ADD]

---
+++ test_pmad_link/__manifest__.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Test PMAD Link",
+    'summary': "Test PMAD Link",
+    'description': """
+Test PMAD Link
+    """,
+    'category': 'Sales/ECommerce',
+    'version': '1.0',
+    'author': 'Odoo IN',
+    'license': 'OPL-1',
+}
